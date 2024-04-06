3bbdd9e0ed7e16c7db9d25b274c9a13fd8f562b6
Sergio Riveros <sergio.riveros@adidas.com>
2024-04-06 13:25:25 -0500

    Adding local database for testing

--- Backend/training_management/src/main.py
+++ Backend/training_management/src/main.py
@@ -1,11 +1,27 @@
 from flask import Flask
+from extensions import db
 from api.training_plan import training_plan_blueprint
+
+# Configuration
+DATABASE_URI = 'sqlite:///trainingmanagement.db'
 
 def create_app():
     app = Flask(__name__)
+
+    # Database configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+    # Initialize app with SQLAlchemy
+    db.init_app(app)
+
+    # Register blueprints
     app.register_blueprint(training_plan_blueprint)
+
     return app
 
 if __name__ == "__main__":
     app = create_app()
+    with app.app_context():
+        db.create_all()  # Create database tables for our data models
     app.run(debug=True)
--- Backend/training_management/src/models/training_plan.py
+++ Backend/training_management/src/models/training_plan.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from extensions import db
 
 class TrainingPlan(db.Model):
     id = db.Column(db.Integer, primary_key=True)

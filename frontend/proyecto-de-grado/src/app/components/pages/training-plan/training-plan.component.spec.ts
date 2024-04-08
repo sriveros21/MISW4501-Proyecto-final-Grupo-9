414b01b4c0db3a561cff041a325715e70ac99233
CristinaGM4 <m.guapacha@uniandes.edu.co>
2024-04-07 19:46:14 -0500

    configuración inicial HU-010

---
+++ frontend/proyecto-de-grado/src/app/components/pages/training-plan/training-plan.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrainingPlanComponent } from './training-plan.component';
+
+describe('TrainingPlanComponent', () => {
+  let component: TrainingPlanComponent;
+  let fixture: ComponentFixture<TrainingPlanComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TrainingPlanComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TrainingPlanComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

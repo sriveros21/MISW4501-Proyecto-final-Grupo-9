90ccf5463e5a3d5f914c1648d18bd259e5c349cd
CristinaGM4 <m.guapacha@uniandes.edu.co>
2024-04-14 11:26:48 -0500

    crear plan de entrenamiento

--- frontend2/SportApp/src/app/plantraining/plantraining.component.ts
+++ frontend2/SportApp/src/app/plantraining/plantraining.component.ts
@@ -1,19 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TrainingPlanService } from '../services/training-plan.service';
 
 @Component({
   selector: 'app-plantraining',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './plantraining.component.html',
   styleUrls: ['./plantraining.component.css']
 })
 export class PlantrainingComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+  mostrarAlerta: boolean = false;
+
+  constructor(private fb: FormBuilder, private trainingPlanService: TrainingPlanService) {
+    this.formulario = this.fb.group({
+      description: ['', Validators.required],
+      exercises: ['', Validators.required],
+      duration: ['', Validators.required],
+      frequency: ['', Validators.required],
+      objectives: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  enviarFormulario() {
+    if (this.formulario.valid) {
+      console.log(this.formulario);
+      const formData = this.formulario.value;
+      this.trainingPlanService.createPlan(formData).subscribe(
+        (respuesta) => {
+          console.log('Datos enviados correctamente:', respuesta);
+          this.formulario.reset();
+          this.mostrarAlerta = true; 
+        },
+        (error) => {
+          console.error('Error al enviar datos:', error);
+        }
+      );
+    } else {
+      console.error('El formulario no es válido. Por favor, completa todos los campos.');
+    }
   }
 
 }
--- frontend2/SportApp/src/app/services/training-plan.service.ts
+++ frontend2/SportApp/src/app/services/training-plan.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,17 @@ export class TrainingPlanService {
 
   getTrainings(): Observable<any> {
     return this.http.get<any>('http://127.0.0.1:5000/training-plans');
+  }
+
+  createPlan(formData: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>('http://127.0.0.1:5000/training-plan', formData, { headers })
+      .pipe(
+        catchError(error => {
+          console.error('Error al enviar datos:', error);
+          return throwError(error);
+        })
+      );
   }
 
 }
